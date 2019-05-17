366108e90c85cd3b53c9297ec392149e9358612f
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2019-05-17 04:43:01 -0400

    simplify module

--- module/{{cookiecutter.project}}/tests/{{cookiecutter.test_package_dir}}/test_{{cookiecutter.module}}.py
+++ module/{{cookiecutter.project}}/tests/{{cookiecutter.test_package_dir}}/test_{{cookiecutter.module}}.py
@@ -5,11 +5,7 @@ from {{cookiecutter.project_name}}.constants import *
 import unittest
 import logging
 
-from {{cookiecutter.project_name}} import cfg
-from {{cookiecutter.project_name}} import util
-from {{cookiecutter.project_name}}.util import Error
 from {{cookiecutter.pkg_name}} import {{cookiecutter.module}}
-from tests import Object
 
 
 class Test{{cookiecutter.Module}}(unittest.TestCase):
