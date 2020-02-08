62d62b1d5eab75884050f49eaf68e3a47aa0de3e
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2020-02-07 19:39:40 -0500

    add prompt

--- scripts/init_dev.sh
+++ scripts/init_dev.sh
@@ -10,20 +10,20 @@ fi
 
 the_basename=`basename \`pwd\``
 
-echo "virtualenv_dir: ${virtualenv_dir} the_basename: ${the_basename}"
+echo "\033[1;33m[INFO]\033[m virtualenv_dir: ${virtualenv_dir} the_basename: ${the_basename}"
 
 # virtualenv
 if [ ! -d ${virtualenv_dir} ]
 then
-  echo "no ${virtualenv_dir}. will create one"
+  echo "\033[1;33m[INFO]\033[m no ${virtualenv_dir}. will create one"
   virtualenv -p `which python3` --prompt="[${the_basename}] " "${virtualenv_dir}"
 fi
 
 source ${virtualenv_dir}/bin/activate
 the_python_path=`which python`
-echo "python: ${the_python_path}"
+echo "\033[1;33m[INFO]\033[m python: ${the_python_path}"
 
-echo "current_dir: "
+echo "\033[1;33m[INFO]\033[m current_dir: "
 pwd
 
 # cp all to current dir
@@ -39,6 +39,7 @@ then
 fi
 
 # requirements-dev.txt
+echo "\033[1;33m[INFO]\033[m requirements-dev:"
 if [ ! -f requirements-dev.txt ]
 then
     cp .cc/requirements-dev.txt requirements-dev.txt
@@ -46,7 +47,12 @@ fi
 pip install -r requirements-dev.txt
 
 # project_dev
+echo "\033[1;33m[INFO]\033[m to init project"
 ./scripts/project_dev.sh
 
 # git init
+echo "\033[1;33m[INFO]\033[m to git init"
 git init; git add .; git commit -m "init dev"
+
+# done
+echo "\033[1;33m[INFO]\033[m done"
